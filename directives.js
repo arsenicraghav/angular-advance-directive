e96ac471370be0db339d9b12fac2116e9cc7541f
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2017-03-28 14:12:20 +0530

    initial import

---
+++ directives.js
@@ -0,0 +1,10 @@
+//   <grid-screen resource="/api/data.json">
+//     <grid-columns>
+//       <grid-column title="Product"     field="product"></grid-column>
+//       <grid-column title="Description" field="description"></grid-column>
+//       <grid-column title="Cost"        field="cost"></grid-column>
+//     </grid-columns>
+//     <grid with-inline-editor></grid>
+//   </grid-screen>
+
+// 3 domain objects: editor, edit, columns, rows
