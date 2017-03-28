35902714db20c9774127081d3385475797c13bf8
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2017-03-28 15:22:04 +0530

    initial directive structure

--- directives.js
+++ directives.js
@@ -8,3 +8,53 @@
 //   </grid-screen>
 
 // 3 domain objects: editor, edit, columns, rows
+
+angular.module("app").directive("gridScreen",function($http){
+    return {
+        restrict : "E",
+        link: function(scope, element, attributes){
+            $http.get(attributes.resource).success(function(response){
+                scope.rows = response.data;
+                console.log('success');
+            })
+            console.log('grid screen');
+        }
+    };
+});
+
+angular.module("app").directive("gridColumns", function(){
+    return {
+        restrict : "E",
+        link: function(){
+            console.log('linked gridColumns');
+        }
+    }
+});
+
+angular.module("app").directive("gridColumn", function(){
+    return {
+        restrict : "E",
+        link: function(scope, element, attributes){
+            console.log('linked gridColumn: ' +  attributes.title);
+        }
+    }
+});
+
+angular.module("app").directive("grid", function(){
+    return {
+        restrict : "E",
+        link: function(){
+            console.log('linked  grid');
+        }
+    }
+});
+
+angular.module("app").directive("withInlineEditor",function (){
+    return {
+        restrict : "A",
+        link: function(){
+            console.log('linked withInlineEditor');
+        }
+    }
+});
+
